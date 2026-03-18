8d82d01a0df9bb4514a107d5864bc05092470602
Jack20 <chunjcsx20@vip.qq.com>
2026-03-18 22:18:07 +0800

    标记文件夹为 Python 包

---
+++ __init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+代码安全审计工具
+"""
+from .core.config import Config, AuditResult, Vulnerability
+from .core.audit_engine import AuditEngine
+from .analyzers import SourceAnalyzer, SinkAnalyzer, CallChainAnalyzer
+from .generators import PoCGenerator
+from .reports import ReportGenerator
+
+__version__ = '1.0.0'
+__author__ = 'Code Audit Tool'
+
+__all__ = [
+    'Config',
+    'AuditResult',
+    'Vulnerability',
+    'AuditEngine',
+    'SourceAnalyzer',
+    'SinkAnalyzer',
+    'CallChainAnalyzer',
+    'PoCGenerator',
+    'ReportGenerator'
+]
